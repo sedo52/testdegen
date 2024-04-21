c57114c5faa2aeb7f1260f6f9b72182270929f15
sedo52 <118020177+sedo52@users.noreply.github.com>
2024-04-21 10:13:26 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -72,6 +72,6 @@ def index():
     return render_template('index.html', result=None)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+   
+    app.run(host='0.0.0.0', port=5000)
    
